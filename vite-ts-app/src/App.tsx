74f65d03a92ddaead08fc27bb7ea4c5547039ce4
mustafaberk-ay <mustafaberkay5234@gmail.com>
2023-12-09 13:09:49 +0100

    update folder structure

---
+++ vite-ts-app/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <h3>Hello from Vite TS App</h3>
+    </div>
+  )
+}
+
+export default App
